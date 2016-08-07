889dcf79be8c6a9c870e906f17a7d0d4a4429a4e
Shray Gahallot <Shray@Shrays-MacBook-Air.local>
2016-08-07 14:27:59 +0530

    Adding search

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -73,9 +73,10 @@ def post_facebook_message(fbid, recevied_message):
         joke_text = 'Yo ' + reply_text
     
 
-    random_quote = return_random_quote()
-    joke_text = return_random_quote()[0] 
-    
+    #random_quote = return_random_quote()
+    #joke_text = return_random_quote()[0] 
+    joke_text = quote_search(recevied_message)
+    response_text = recevied_message +' :)'
 
     message_object = {
         "attachment":{
@@ -87,8 +88,15 @@ def post_facebook_message(fbid, recevied_message):
     }
     print "Posting message"               
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+    #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+    response_msg2 = json.dumps({"recipient":{"id":fbid}, "message":{"text":response_text}})
+    
+    response_msg3 = json.dumps({"recipient":{"id":fbid}, "message": message_object})
+    
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    #status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg2)
+    #status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
 
