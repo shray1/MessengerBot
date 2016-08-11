47cee8d28edb44c6df2393aa43fcae517c45c830
Shray Gahallot <Shray@Shrays-MacBook-Air.local>
2016-08-11 22:17:32 +0530

    Different types of messages

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -18,6 +18,8 @@ from django.utils.decorators import method_decorator
 PAGE_ACCESS_TOKEN = 'EAAQA1ZA0bZBB0BAKHYZA0AIgslS75KYQVheP0eIH6r5JKXZArYl3FWJzAksixxZBpP55BqfSUavhOQ8PhaHcM4kovmJQNTmfdkdQKkjup0wN9u9Yf8PSiB6ydAc9BSex5KjKQqaKWwuXqNOid9eRWwFIZBsFoXinZAVa6z5rwnN7wZDZD'
 VERIFY_TOKEN = '8447208288'
 
+
+
 quotes_arr = [["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
 ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
 ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
@@ -52,12 +54,7 @@ def quote_search(str_var):
             return quote_text
 
     return return_random_quote()[0]
-'''def quote_search(str_var):
-    for quote_text , quote_author in quotes_arr:
-        if str_var in quote_author.lower():
-            return quote_text
-    return return_random_quote()
-'''
+
 
 def post_facebook_message(fbid, recevied_message):
     reply_text = recevied_message + ' :)'
@@ -73,11 +70,11 @@ def post_facebook_message(fbid, recevied_message):
         joke_text = 'Yo ' + reply_text
     
     #joke_text = quote_search(recevied_message)
-    joke_text = return_random_quote()[0] 
-    response_text = recevied_message +' :)'
+    #joke_text = return_random_quote()[0] 
+    #response_text = recevied_message +' :)'
 
 
-    message_object = {
+    message_image = {
         "attachment":{
           "type":"image",
           "payload":{
@@ -87,48 +84,43 @@ def post_facebook_message(fbid, recevied_message):
         }
     }
 
-    message_object2 = {
+    message_text = {
         "text": joke_text
         }
-                   
+    
+
+    message_generic_template = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"generic",
+                "elements":[
+                  {
+                    "title":"Welcome to Sec 23 Giving Back Society",
+                    "image_url":"http://worldversus.com/img/ironman.jpg",
+                    "subtitle":"We\'ve got the right hat for everyone.",
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"https://google.com/",
+                        "title":"View Website"
+                      } ]}]}}}
+
+
+
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    response_msg2 = json.dumps({"recipient":{"id":fbid}, "message":{"text":response_text}})
+    response_txt_msg = json.dumps({"recipient":{"id":fbid}, "message":message_text})
+    response_img_msg = json.dumps({"recipient":{"id":fbid}, "message":message_image})
+    response_temp_msg = json.dumps({"recipient":{"id":fbid}, "message":message_generic_template})
+
+    #response_msg3 = json.dumps({"recipient":{"id":fbid}, "message": message_object})
     
-    response_msg3 = json.dumps({"recipient":{"id":fbid}, "message": message_object})
-    
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_txt_msg)
     #status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg2)
-    
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_img_msg)
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_temp_msg)
     pprint(status.json())
 
-'''
-    #random_quote = return_random_quote()
-    #joke_text = return_random_quote()[0] 
-    joke_text = quote_search(recevied_message)
-    response_text = recevied_message +' :)'
-
-    message_object = {
-        "attachment":{
-          "type":"image",
-          "payload":{
-            "url":"https://petersapparel.com/img/shirt.png"
-          }
-        }
-    }
-    print "Posting message"               
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    response_msg2 = json.dumps({"recipient":{"id":fbid}, "message":{"text":response_text}})
-    
-    response_msg3 = json.dumps({"recipient":{"id":fbid}, "message": message_object})
-    
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    #status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg2)
-    #status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    pprint(status.json())
-'''
 
 class MyQuoteBotView(generic.View):
     def get(self, request, *args, **kwargs):
@@ -181,85 +173,6 @@ def test():
 
 
 
-'''
-
-
-import json, requests, random, re
-from pprint import pprint
-
-
-from django.shortcuts import render
-
-from django.views import generic
-from django.http.response import HttpResponse
-
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-
-# Create your views here.
-
-PAGE_ACCESS_TOKEN = 'EAAQA1ZA0bZBB0BAJfRrVXYdjHR0mhH8aDUfFGNBjhynrcCiHmyZCGSE11LxLbRPPadrmLTHNcEZA9ZCXvs4izgaNhXkZBVkWxvZA8CK7vP5RqOmK4k3Ah24DWooWw2o6ZBGHnZCEvms8RXKD5vZBFwQ81ZCFZAT1PnEmvXE8MIHZBHgnLRwZDZD'
-
-
-
-def post_facebook_message(fbid, recevied_message):
-    # Remove all punctuations, lower case the text and split it based on space
-    #clean_message = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message)
-
-    reply_text = ''
-    
-
-    if not reply_text:
-        reply_text = recevied_message
-
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
-    user_details = requests.get(user_details_url, user_details_params).json() 
-    joke_text = 'Yo '+user_details['first_name']+'..! ' + reply_text
-                   
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    pprint(status.json())
-
-
-class MyQuoteBotView(generic.View):
-    def get(self, request, *args, **kwargs):
-        if self.request.GET['hub.verify_token'] == '8447208288':
-            return HttpResponse(self.request.GET['hub.challenge'])
-        else:
-            return HttpResponse('Error, invalid token')
-        
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return generic.View.dispatch(self, request, *args, **kwargs)
-
-    # Post function to handle Facebook messages
-    def post(self, request, *args, **kwargs):
-        # Converts the text payload into a python dictionary
-        incoming_message = json.loads(self.request.body.decode('utf-8'))
-        # Facebook recommends going through every entry since they might send
-        # multiple messages in a single call during high load
-        for entry in incoming_message['entry']:
-            for message in entry['messaging']:
-                # Check to make sure the received call is a message call
-                # This might be delivery, optin, postback for other events 
-                if 'message' in message:
-                    # Print the message to the terminal
-                    pprint(message)    
-                    # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
-                    # are sent as attachments and must be handled accordingly. 
-                    post_facebook_message(message['sender']['id'], message['message']['text'])    
-        return HttpResponse()    
-
-
-def index(request):
-    return HttpResponse("Hello World")
-
-
-def hello(request):
-    return HttpResponse('By Bye world')
-    '''
 
 
 
